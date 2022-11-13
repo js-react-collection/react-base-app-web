bd99cd4e2b6b1d321ac5a3c77f6162f3f7bdbe4c
Bertz <bertz.connect@gmail.com>
2022-11-13 11:48:48 +0100

    ...

---
+++ testcomponent.js
@@ -0,0 +1,11 @@
+export default function TestComponent(props) {
+    return make('h1', null, `Hello World!`)
+}
+
+// export default function TestComponent () {
+//     return (
+//         <div className="view">
+//             <h2>This is imported component!</h2>
+//         </div>
+//     )
+// }
